faef603dad7d98f888c9ba1045c74c4909702f71
michmitch <62762271+michmitch@users.noreply.github.com>
2021-08-28 00:39:44 +0700

    Create driver_conn.php

---
+++ php-api/driver_conn.php
@@ -0,0 +1,13 @@
+<?php
+$HostName = "localhost";
+ 
+//Define your MySQL Database Name here.
+$DatabaseName = "tahuenak_tahuenak";
+ 
+//Define your Database User Name here.
+$HostUser = "tahuenak_admin";
+ 
+//Define your Database Password here.
+$HostPass = "AdminTahuEnak";
+
+?>
